6bdfc3a36a44c44923620c3d23bd9bff963d64f7
Taimoor <smartyy906@gmail.com>
2024-04-04 06:22:33 +0500

    newsletter content fixed

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -37,8 +37,8 @@ export default function Newsletter() {
 
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
-                <h3 className="h3 text-white mb-2">Want more tutorials & guides?</h3>
-                <p className="text-gray-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit nemo expedita voluptas culpa sapiente.</p>
+                <h3 className="h3 text-white mb-2">Want news regaring Plura AI - Subscribe Now!</h3>
+                <p className="text-gray-300 text-lg mb-6">Stay Connected, Stay Informed</p>
 
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
